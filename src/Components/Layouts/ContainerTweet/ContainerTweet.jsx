906bd573d905afcc959c2007857176d8291632d2
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-02-24 15:52:54 -0500

    Se agrego el contador

--- src/Components/Layouts/ContainerTweet/ContainerTweet.jsx
+++ src/Components/Layouts/ContainerTweet/ContainerTweet.jsx
@@ -5,32 +5,42 @@ export const ContainerTweet = () => {
 
   const [text, setText] = useState('');
   const [assignedText, setAssignedText] = useState('Aquí verás tu tweet actual');
+  
+  const [valueQ, setValue] = useState(0)
+  const maxLength = 254
+
 
   const handleTextAreaChange = (e) =>{
-    setText(e.target.value)
+    const value = e.target.value;
+    
+    if (value.length <= maxLength) {
+      setText(value);
+    }
+      setValue(value.length)
   }
 
   const handleAssignClick  = () =>{
     setAssignedText(text);
     setText('');
-    localStorage.setItem('textArea', text);
+    setValue(0)
   }
 
-  const asi = () =>{
-    let local =  localStorage.getItem('textArea')
-    alert(local)
+  const handleAssignClick2 = () =>{
+
   }
+
 
   return (
     <div className='containerTweet'>
         <h2 className='info'>Publica tu tweet</h2>
         <hr />
-        <textarea value={text} onChange={handleTextAreaChange}  placeholder='Escribe un tweet(max 255 caracteres)' name=""  cols="30" rows="10" maxLength='255' className='containText'></textarea>
+        <textarea value={text} onInput={handleTextAreaChange} placeholder='Escribe un tweet(max 255 caracteres)' name=""  cols="30" rows="10"  className='containText'></textarea>
         <div className='containerBtns'>
           <ButtonUI event={handleAssignClick } style="btns-action" textButton="Publicar" />
-          <ButtonUI event={asi} style="btns-action" textButton="Archivar" />
-          <ButtonUI event="" style="btns-action" textButton="Mostrar Archivados" />
+          <ButtonUI event={handleAssignClick2} style="btns-action" textButton="Archivar" />
+          <ButtonUI event={() => {console.log("hola")}} style="btns-action" textButton="Mostrar Archivados" />
         </div>
+        <p className='quantity'>{valueQ}</p>
         <p className='tweets' >{assignedText}</p>
     </div>
   )
