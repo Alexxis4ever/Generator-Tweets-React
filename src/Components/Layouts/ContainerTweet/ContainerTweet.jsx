2f89491785966808c4b49a05f40a21d056ddfe10
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-02-28 22:26:11 -0500

    Se cambio por useRef y se termino

--- src/Components/Layouts/ContainerTweet/ContainerTweet.jsx
+++ src/Components/Layouts/ContainerTweet/ContainerTweet.jsx
@@ -1,70 +1,50 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ButtonUI } from '../../UI/ButtonUI/ButtonUI'
 import { ListaTweets } from '../../UI/ListaTweets/ListaTweets';
 
-const arrayTweets = []
+let arrayTweets = []
 
 export const ContainerTweet = () => {
 
-  const [text, setText] = useState('');
+  const refTA = useRef();
+
   const [assignedText, setAssignedText] = useState('Aquí verás tu tweet actual');
-
-  const [valueQ, setValue] = useState(255)
-  const maxLength = 255
-
- 
-  // Hacemos uso del useState y lo establecemos 
-  // con el estado inicial de un arreglo
   const [tweet, setTweet] = useState([]);
-
-
-  // Función para manipular datos del textarea
-  const textAreaManagement = (e) => {
-    const value = e.target.value
-      setText(value);
-  }
+  const [letters, setLetters] = useState(255);
+  const [colorLt, setColorLt] = useState('counter');
+  
 
   // Función para el boton de Publicar
   const postButton = () => {
-      setAssignedText(text);
-      setText('');
-      setValue(0)
+    setAssignedText(refTA.current.value)
   }
 
 
   // Función para el boton de Archivar
   const archiveButton = () => {
-    let save = assignedText
-    arrayTweets.push(save)
-    localStorage.setItem("dates", arrayTweets)
+    arrayTweets.push(refTA.current.value);
+    localStorage.setItem("tweets", arrayTweets);
   }
 
   // Función para el boton de MostrarArchivados
   const showFiles = () => {
-      const localTweets = localStorage.getItem("dates")
-
-      setTweet([...tweet, localTweets])
+      const tweetList = localStorage.getItem("tweets");
+      const data = tweetList.split(',');
+      setTweet(data);
   }
 
+   // Función para manipular datos del textarea
+   const textAreaManagement = () => {
+    setLetters(letters-1);
+}
 
-  // llamar la funcion
-  const count = () =>{
-    setValue(valueQ - 1)
-  }
-
-  // Utilizo useEffect para visualizar en consola
-  // los datos de mi arreglo
-
-  // useEffect(() =>{
-  //   console.log(tweet);
-  // }, [tweet]);
 
   return (
     <>
       <div className='generalContainer'>
         <h2 className='info'>Publica tu tweet</h2>
         <hr />
-        <textarea className='containText' value={text} onChange={textAreaManagement} placeholder='Escribe un tweet(max 255 caracteres)' cols="30" rows="10"></textarea>
+        <textarea ref={refTA} className='containText' onChange={textAreaManagement} placeholder='Escribe un tweet(max 255 caracteres)' cols="30" rows="10"></textarea>
 
         <div className='containerBtns'>
           <ButtonUI event={postButton} style="btns-action" textButton="Publicar" />
@@ -72,7 +52,7 @@ export const ContainerTweet = () => {
           <ButtonUI event={showFiles} style="btns-action" textButton="Mostrar Archivados" />
         </div>
 
-        <p className='quantity'>{valueQ}</p>
+        <p className={colorLt}>{letters}</p>
 
         <div className='tweetsContainer'>
           <p className='tweets' >{assignedText}</p>
